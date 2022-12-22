bf12de26521ca9baf1668b516d4c590dbd25ee85
Raiff Said <raiff.said@gmail.com>
2022-12-22 17:45:33 -0300

    env: Adjust initial data for preview

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,7 @@
 import { Item } from "../types/Item";
 
 export const items: Item[] = [
-  { date: new Date(2022, 11, 17), category: 'expense', title: 'McDonalds', value: 32.12 },
-  { date: new Date(2022, 11, 17), category: 'expense', title: 'Burger King', value: 28 },
-  { date: new Date(2022, 11, 19), category: 'expense', title: 'Aluguel Apt', value: 2300 },
-  { date: new Date(2022, 10, 18), category: 'income', title: 'Salário', value: 4500 }
+  { date: new Date(2022, 11, 10), category: 'income', title: 'Salário', value: 5000 },
+  { date: new Date(2022, 11, 23), category: 'expense', title: 'Cartão de crédito', value: 4038.18 },
+  { date: new Date(2022, 11, 17), category: 'expense', title: 'Pagamento empréstimo', value: 500 }
 ];
