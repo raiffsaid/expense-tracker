bfb3d20b87283287adeb38711395049522d1db8d
Raiff Said <raiff.said@gmail.com>
2022-12-28 19:09:43 -0300

    env: Adjusted mongodb configuration and User entity.

---
+++ backend/src/main/java/com/raibratech/expensetracker/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.raibratech.expensetracker.repositories;
+
+import com.raibratech.expensetracker.models.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
