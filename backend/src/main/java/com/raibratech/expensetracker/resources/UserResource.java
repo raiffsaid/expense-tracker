95ca0dff3d85efd7686658f201b11a0e07f41b8e
Raiff Said <raiff.said@gmail.com>
2022-12-29 00:00:10 -0300

    feat: User POST

--- backend/src/main/java/com/raibratech/expensetracker/resources/UserResource.java
+++ backend/src/main/java/com/raibratech/expensetracker/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.raibratech.expensetracker.models.dto.UserDTO;
 import com.raibratech.expensetracker.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +26,12 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         UserDTO obj = userService.findById(id);
         return ResponseEntity.ok(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
