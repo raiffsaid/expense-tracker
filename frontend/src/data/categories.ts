4cd82042d107bcb07f9e202e0940a866e1f8f36b
Raiff Said <raiff.said@gmail.com>
2023-01-09 12:34:23 -0300

    chore: Add new category

--- frontend/src/data/categories.ts
+++ frontend/src/data/categories.ts
@@ -2,5 +2,6 @@ import { Category } from "../types/Category";
 
 export const categories: Category = {
   income: { title: 'Receita', color: 'blue', expense: false },
-  expense: { title: 'Despesa', color: 'brown', expense: true }
+  expense: { title: 'Despesa', color: 'brown', expense: true },
+  investment: { title: 'Investimento', color: 'green', expense: true }, 
 };
